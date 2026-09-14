d2cd77f74f6f46e0ae1a08c32445a47ac3dd67cc
claudia <claudiabeltran.apr@gmail.com>
2026-09-14 11:28:01 +0200

    fix: fix current skins

--- renderer.js
+++ renderer.js
@@ -143,7 +143,7 @@ exitBtn.addEventListener('click', () => {
 })
 
 // ---- SKINS ----
-const skins = ['old-clock', 'breakfast-time', 'growing-plant', 'calendar'];
+const skins = ['default-timer', 'banana-breakfast', 'blueberry-breakfast'];
 let currentSkinIndex = 0;
 
 const prevSkinBtn = document.getElementById('prev-skin');
